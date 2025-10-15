94174db757e45480ece8da047052118b84deab0b
sachin-09012006 <jalasachin93@gmail.com>
2025-10-15 18:57:28 +0530

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -117,6 +117,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR/'static/images'
+MEDIAFILES_DIRS = [BASE_DIR/'static/images']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
